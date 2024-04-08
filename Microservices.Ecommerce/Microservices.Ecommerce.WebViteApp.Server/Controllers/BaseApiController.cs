50005bab620ea0c8ea46ce623f66b727fba5da5c
Le Anh Toan <leanhtoan@vietbank.com.vn>
2024-04-08 17:06:47 +0700

    Add Account controller

---
+++ Microservices.Ecommerce/Microservices.Ecommerce.WebViteApp.Server/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+﻿using MassTransit.Mediator;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservices.Ecommerce.WebViteApp.Server.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public abstract class BaseApiController : ControllerBase
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
