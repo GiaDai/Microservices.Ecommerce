6a45c14ae8d9655e31be2c18c22d0bd978129469
Le Anh Toan <leanhtoan@vietbank.com.vn>
2024-05-04 08:39:10 +0700

    integrate refine framework

--- Microservices.Ecommerce/microservices.ecommerce.webviteapp.client/src/App.tsx
+++ Microservices.Ecommerce/microservices.ecommerce.webviteapp.client/src/App.tsx
@@ -1,10 +1,23 @@
+import { ErrorComponent } from "@refinedev/antd";
+import { Refine, WelcomePage } from "@refinedev/core";
+import { ConfigProvider, App as AntdApp } from "antd";
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <BrowserRouter>
+      <ConfigProvider>
+        <AntdApp>
+          <Refine>
+            <Routes>
+              <Route path="/" element={<WelcomePage />} />
+              <Route path="*" element={<ErrorComponent />} />
+            </Routes>
+          </Refine>
+        </AntdApp>
+      </ConfigProvider>
+    </BrowserRouter>
   );
 };
 
