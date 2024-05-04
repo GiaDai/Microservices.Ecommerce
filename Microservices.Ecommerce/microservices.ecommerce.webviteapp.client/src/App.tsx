83c17b42ae98ef9e3386fcdedeb45ec66ebff578
Le Anh Toan <leanhtoan@vietbank.com.vn>
2024-05-04 08:18:14 +0700

    reset reactjs

--- Microservices.Ecommerce/microservices.ecommerce.webviteapp.client/src/App.tsx
+++ Microservices.Ecommerce/microservices.ecommerce.webviteapp.client/src/App.tsx
@@ -1,63 +1,11 @@
-import { lazy, useEffect } from "react";
-import { themeChange } from 'theme-change'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { QueryProvider } from './api/common/query-provider';
-import { hydrateAuth, handleBeforeUnload, destroyBeforeUnload } from "./core";
-import { useSelector, useDispatch } from 'react-redux'
-import { ToastContainer, toast } from 'react-toastify';
-import  {  clearToast } from "@features/common/toastSlice"
-import PrivateRouteWrapper from './routes/PrivateRoute';
+import React from "react";
 
-const Layout = lazy(() => import('./containers/Layout'))
-const Login = lazy(() => import('./pages/Login'));
-const Register = lazy(() => import('./pages/Register'));
-const ForgotPassword = lazy(() => import('./pages/ForgotPassword'));
-const Documentation = lazy(() => import('./pages/Documentation'));
-
-
-const App = () => {
-    const dispatch = useDispatch();
-    const message = useSelector((state: any) => state.toast.message);
-    const type = useSelector((state: any) => state.toast.type);
-    useEffect(() => {
-        // 👆 daisy UI themes initialization
-        hydrateAuth();
-        handleBeforeUnload();
-        themeChange(false);
-        return () => {
-            destroyBeforeUnload();
-        }
-      }, [])
-      useEffect(() => {
-        if(message !== ""){
-            if(type === "success")toast.success(message)
-            if(type === "error")toast.error(message)
-            if(type === "info")toast.info(message)
-            if(type === "warning")toast.warn(message)
-            dispatch(clearToast())
-            // dispatch(removeNotificationMessage())
-        }
-    }, [message])
-    return (
-        <>
-        <QueryProvider>
-            <Router>
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/forgot-password" element={<ForgotPassword />} />
-                    <Route path="/documentation" element={<Documentation />} />
-                    {/* Place new routes over this */}
-                    <Route path="/app/*" element={<PrivateRouteWrapper><Layout /></PrivateRouteWrapper>} />
-                </Routes>
-            </Router>
-        </QueryProvider>
-        <ToastContainer 
-            hideProgressBar={true}
-            theme="colored"
-        />
-        </>
-    );
-}
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+};
 
 export default App;
--- Microservices.Ecommerce/microservices.ecommerce.webviteapp.client/src/main.tsx
+++ Microservices.Ecommerce/microservices.ecommerce.webviteapp.client/src/main.tsx
@@ -1,16 +1,9 @@
-import {Suspense} from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import 'react-toastify/dist/ReactToastify.css'
-import store from './app/store'
-import { Provider } from 'react-redux'
-import SuspenseContent from './containers/SuspenseContent';
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Suspense fallback={<SuspenseContent loadingText='Loading' />}>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </Suspense>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
